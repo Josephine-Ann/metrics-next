cd7beeff3ed462de27ac068bd518be6e06a1dd03
josephine geoghegan <josephineanngeoghegan@gmail.com>
2022-05-13 13:58:28 +0200

    invisible

---
+++ pages/invisible.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Navbar from '../Components/Navbar';
+
+export default function BeaconMessage() {
+
+
+    return (
+        <Navbar>
+            <div className='lg:w-[50vw] lg:h-[91vh] h-[41vh]'>
+                <h1 class="text-[6.5vh] text-white">Yo</h1>
+            </div>
+            <div className='lg:w-[50vw] lg:h-[91vh] h-[50vh]'>
+                <div className='h-[50%] w-[40vw] lg:w-[20vw]  bg-[#d7c9c9]'>
+
+                </div>
+            </div>
+            <div className='w-[20%] h-full'></div>
+            <p className="text-[4vw] text-white m-4 text-lg"></p>
+            <div className='w-[80%] h-full flex-col'></div>
+            <div className="text-[7.5vh] md:text-[9.5vh] text-[#594343]"></div>
+            <p className="text-[#594343] md:text-2xl">Hi</p>
+            <div className="text-[7.5vh] md:text-[9.5vh] text-white"></div>
+            <p className="text-white  md:text-2xl">Hee</p>
+            <div className="w-[60vw] lg:w-[30vw]  bg-[#4a3244]"></div>
+            <div className="w-[60%] h-full items-end flex-col"></div>
+        </Navbar>
+
+    )
+}
+
